92f91952be54bfc2a341c43da5e9f4cad7d0491d
RTiberiu <72140679+RTiberiu@users.noreply.github.com>
2020-11-29 21:03:13 +0000

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
       $('body').show();
       console.log(window.location.href);
       // Add index.html at the end of the URL
-      if (window.location.href === 'https://filmsociety-rgu.netlify.app') {
+      if (window.location.href === 'https://filmsociety-rgu.netlify.app/') {
          window.location = 'https://filmsociety-rgu.netlify.app/index.html';
       };
 
